73ee9c81065e8f1ce9a3190bdb766f5dc8c5a8ae
John Huh <jhuh014@wch136-23.cs.ucr.edu>
2019-10-04 12:07:14 -0700

    Completed exercise 3

---
+++ turnin/jhuh014_lab2_part3.c
@@ -0,0 +1,60 @@
+/*	Author: jhuh014
+ *  Partner(s) Name: Alex DeVictoria
+ *	Lab Section: 23
+ *	Assignment: Lab 2  Exercise 3
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00; // Initialize PORTC output to 0’s
+    unsigned char tmpB = 0x00;
+    unsigned char tmpA = 0x00;
+    unsigned char tmpC = 0x00;
+    unsigned char tmpD = 0x00;
+    unsigned char tmpE = 0x00;
+    unsigned char cntavail = 0x00;
+    
+    /* Insert your solution below */
+    while (1) {
+        tmpA = PINA & 0x01;
+        tmpB = PINA & 0x02;
+        tmpC = PINA & 0x04;
+        tmpD = PINA & 0x08;
+        tmpE = PINA & 0x0F;
+        cntavail = 0x00;
+
+        if (tmpE == 0x0F) {
+            cntavail = 0x80;
+        }
+
+        else {
+
+            if (tmpA != 0x01) {
+                cntavail = cntavail + 1;
+            }
+
+            if (tmpB != 0x02) {
+                cntavail = cntavail + 1;
+            }
+
+            if (tmpC != 0x04) {
+                cntavail = cntavail + 1;
+            }
+
+            if (tmpD != 0x08) {
+                cntavail = cntavail + 1;
+            }
+        }
+        PORTC = cntavail;
+    }
+    return 0;
+}
